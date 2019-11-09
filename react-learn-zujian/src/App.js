7225e92ff6453f19b01e7e8e09e34e95b96a17dd
hhf1229 <1322408331@qq.com>
2019-11-09 15:24:13 +0800

    完成动画3的学习

--- react-learn-zujian/src/App.js
+++ react-learn-zujian/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import {CSSTransition,SwitchTransition} from 'react-transition-group'
+import React,{useState} from 'react'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 export default function App() {
+  const [visiable, setvisiable] = useState(true)
   return (
     <div>
-      
+      <SwitchTransition >
+        <CSSTransition timeout={1000}  key={visiable}>
+            <h1>{visiable?'title1':'title2'}</h1>
+        </CSSTransition>
+      </SwitchTransition>
+      <button onClick={()=>{
+        setvisiable(!visiable)
+      }}>切换</button>
     </div>
   )
 }
