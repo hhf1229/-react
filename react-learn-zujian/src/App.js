c8dcb4a4b8f2fa2276ea20333368ad2d7fbe9fd1
hhf1229 <1322408331@qq.com>
2019-11-09 10:40:05 +0800

    动画3的内容

--- react-learn-zujian/src/App.js
+++ react-learn-zujian/src/App.js
@@ -1,26 +1,9 @@
-import React,{useState} from 'react'
-import { CSSTransition } from 'react-transition-group';
-import './App.css'
-
-function Com1({visiable}){
-  return <CSSTransition in={visiable} timeout={1000}>
-    <h1>组件1</h1>
-  </CSSTransition>
-}
-function Com2({visiable}){
-  return <CSSTransition in={visiable} timeout={1000}>
-    <h1>组件2</h1>
-  </CSSTransition>
-}
- export default function App() {
-  const [show, setShow] = useState(true)
+import React from 'react'
+import {CSSTransition,SwitchTransition} from 'react-transition-group'
+export default function App() {
   return (
-    <div className='container'>
-      <Com1 visiable={show}></Com1>
-      <Com2 visiable={!show}></Com2>
-      <button onClick={()=>{
-        setShow(!show)
-      }}>切换</button>
+    <div>
+      
     </div>
-  );
+  )
 }
