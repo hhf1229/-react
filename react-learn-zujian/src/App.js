f46a74baedddbc9bd08e19515f253e8e4a19428a
hhf1229 <1322408331@qq.com>
2019-11-09 16:32:15 +0800

    学习完成

--- react-learn-zujian/src/App.js
+++ react-learn-zujian/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import uuid from 'uuid'
+import './App.css'
+/* 本节课的练习的目的是设置两个任务1和任务2，同时通过删除按钮来删除，通过使用TransitionGroup来包裹CSSTransition */
 export default function App() {
+  const [taskList, settaskList] = useState([
+    { id: uuid(), name: '任务1' },
+    { id: uuid(), name: '任务2' }
+  ])
   return (
     <div>
-      在这里我写了东西
+      <TransitionGroup>
+        {
+          taskList.map(it => (<CSSTransition appear timeout={500} key={it.id}>
+            <div>{it.name}
+              <button onClick={() => {
+                const newTaskList = taskList.filter(t => t.id !== it.id)
+                settaskList(newTaskList)
+              }}>删除</button>
+            </div>
+          </CSSTransition>
+          ))
+        }
+      </TransitionGroup>
+      <button onClick={()=>{
+        const value = window.prompt('请输入一个任务名称')
+        settaskList([...taskList,{id:uuid(),name:value}])
+      }}>添加一个任务</button>
     </div>
   )
 }
