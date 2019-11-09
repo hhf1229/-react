3c8aa1c75aba5840c8088070bc273850e9d7934d
hhf1229 <1322408331@qq.com>
2019-11-09 15:37:01 +0800

    用来克隆空白的分支

--- react-learn-zujian/src/App.js
+++ react-learn-zujian/src/App.js
@@ -1,26 +1,10 @@
-import React,{useState} from 'react'
-import { CSSTransition } from 'react-transition-group';
-import './App.css'
+import React from 'react'
 
-function Com1({visiable}){
-  return <CSSTransition in={visiable} timeout={1000}>
-    <h1>组件1</h1>
-  </CSSTransition>
-}
-function Com2({visiable}){
-  return <CSSTransition in={visiable} timeout={1000}>
-    <h1>组件2</h1>
-  </CSSTransition>
-}
- export default function App() {
-  const [show, setShow] = useState(true)
+export default function App() {
   return (
-    <div className='container'>
-      <Com1 visiable={show}></Com1>
-      <Com2 visiable={!show}></Com2>
-      <button onClick={()=>{
-        setShow(!show)
-      }}>切换</button>
+    <div>
+      
     </div>
-  );
+  )
 }
+
