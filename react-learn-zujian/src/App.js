428ef5641452c02704ef9cc2c8c7dbc53b050baf
hhf1229 <1322408331@qq.com>
2019-11-16 18:11:33 +0800

    test

--- react-learn-zujian/src/App.js
+++ react-learn-zujian/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import {BrowserRouter as Router , Switch,Route} from 'react-router-dom'
-import Login from './components/Pages/Login/index'
-import Admin from './components/Pages/Admin/index'
+
 export default function App() {
   return (
-    <Router>
-      <Switch>
-          <Route exact path='/login' component={Login} />
-          <Route path='/' component={Admin} />
-      </Switch>
-    </Router>
- )
+    <div>
+      
+    </div>
+  )
 }
-
