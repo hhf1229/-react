5f6b07fa97f342d10f84b60f5fb5b06dc1b4e45c
hhf1229 <1322408331@qq.com>
2019-11-18 16:17:53 +0800

    ok;2C

--- react-learn-zujian/src/App.js
+++ react-learn-zujian/src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { BrowserRouter as Router, NavLink, Route, Switch, Redirect } from 'react-router-dom'
 export default function App() {
+  function A() {
+    return <h1>A页面</h1>
+  }
+  function B() {
+    return <h1>B页面</h1>
+  }
+  function Nav() {
+    return <div>
+      <NavLink to='/a'>去a页面</NavLink>
+      <NavLink to='/b'>去b页面</NavLink>
+      <NavLink to='/abc' style={{ marginLeft: 20 }}
+      
+      >其他页</NavLink>
+    </div>
+  }
   return (
     <div>
-      
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path='/a' component={A}></Route>
+          <Route path='/b' component={B}></Route>
+          <Redirect to='/a'></Redirect>
+        </Switch>
+      </Router>
     </div>
   )
 }
