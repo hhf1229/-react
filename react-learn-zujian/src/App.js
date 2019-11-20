4dc018bc079191c5892bf2597d904630da9ca8fd
hhf1229 <1322408331@qq.com>
2019-11-20 10:45:07 +0800

    ok

--- react-learn-zujian/src/App.js
+++ react-learn-zujian/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react'
-
+import {BrowserRouter as Router} from 'react-router-dom'
 export default function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <Router>
+    
+  </Router>
 }
 
