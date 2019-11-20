1b38ffcbb45a3cf923332cc44ca3cd6f5ec2ac3e
hhf1229 <1322408331@qq.com>
2019-11-20 18:01:11 +0800

    ok

--- react-learn-zujian/src/Route.js
+++ react-learn-zujian/src/Route.js
@@ -1,8 +1,10 @@
 import React, { PureComponent } from 'react'
 
 export default class Route extends PureComponent {
-
+  //封装Route的匹配规则，
+  // 参数ctx为上下文的东西，里面有history，location，match的东西
   popei =(ctx)=>{
+    // 如果children属性的值不为空，且为函数的时候，直接渲染，否则直接返回渲染
       if(this.props.children !== undefined){
         if( typeof this.props.children === 'function'){
           return this.props.children(ctx)
@@ -14,11 +16,13 @@ export default class Route extends PureComponent {
         if(!ctx.match){
           return null
         }
+        // 有render函数的时候
         if(this.props.render !== undefined){
           if(typeof this.props.render === 'function'){
             return this.props.render(ctx)
           }
         };
+        // 有component的时候
         if(this.props.component !== undefined){
           const Component = this.props.component;
           return <Component {...ctx} />
@@ -26,11 +30,17 @@ export default class Route extends PureComponent {
         return null
       }
   }
+  ctxValue = {}
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    this.ctxValue.history = this.props.history;
+    this.ctxValue.location = this.props.history.location;
+    this.ctxValue.match = matchPath()
+    return <ctx.Consumer>
+      {value =>{
+        return <ctx.Provider value={this.ctxValue}>
+
+        </ctx.Provider>
+      }}
+    </ctx.Consumer>
   }
 }
